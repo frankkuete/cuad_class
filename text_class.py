28fbe6fd8bf1977ec48e92392ba00531a90cd082
frank <kuete.frank1@gmail.com>
2023-04-14 14:53:47 +0200

    modify text_class

--- text_class.py
+++ text_class.py
@@ -1,9 +1,9 @@
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
-from transformers import AutoTokenizer, DataCollatorWithPadding
-from datasets import load_dataset
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, AutoTokenizer, DataCollatorWithPadding
+from datasets import Dataset, load_dataset
 import json
 import evaluate
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 def compute_metrics(eval_pred):
@@ -21,9 +21,18 @@ loaded_dataset = load_dataset(
 loaded_test_set = load_dataset(
     'json', data_files='cuad_test.json', split="train")
 
+train_dataset, validation_dataset = train_test_split(
+    loaded_dataset, test_size=0.2)
+
+# Convert train and validation datasets to Dataset objects
+train_dataset = Dataset.from_dict(train_dataset)
+validation_dataset = Dataset.from_dict(validation_dataset)
+
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
-tokenized_cuad_train = loaded_dataset.map(preprocess_function, batched=True)
+tokenized_cuad_train = train_dataset.map(preprocess_function, batched=True)
+tokenized_cuad_validation = validation_dataset.map(
+    preprocess_function, batched=True)
 tokenized_cuad_test = loaded_test_set.map(preprocess_function, batched=True)
 
 
@@ -43,7 +52,7 @@ model = AutoModelForSequenceClassification.from_pretrained(
 training_args = TrainingArguments(
     output_dir="distilbert-cuad",
     learning_rate=2e-5,
-    per_device_train_batch_size=8,
+    per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     num_train_epochs=2,
     weight_decay=0.01,
@@ -52,21 +61,11 @@ training_args = TrainingArguments(
     load_best_model_at_end=True,
     push_to_hub=False,
 )
-"""
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=tokenized_cuad_train,
-    eval_dataset=tokenized_cuad_test,
-    tokenizer=tokenizer,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics,
-)
-"""
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_cuad_train,
+    eval_dataset=tokenized_cuad_validation,
     tokenizer=tokenizer,
     data_collator=data_collator,
     compute_metrics=compute_metrics,
