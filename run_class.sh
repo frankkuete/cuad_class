421af5e53dcbc71db93bda2680103096c8ab82d4
frank <kuete.frank1@gmail.com>
2023-07-20 06:18:35 +0200

    .

--- run_class.sh
+++ run_class.sh
@@ -1,7 +1,7 @@
 python run_seq_class.py \
 --model_name "bert-base-uncased" \
---learning_rate 1e-5 \
---num_train_epochs 3 \
+--learning_rate 2e-5 \
+--num_train_epochs 5 \
 --per_device_train_batch_size 24 \
 --per_device_eval_batch_size 16 \
 --train_file "./data/Non-Disparagement/train.csv" \
