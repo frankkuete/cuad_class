7825942ce5c250be0388f5715b9cd8ec136f0f5b
frank <kuete.frank1@gmail.com>
2023-07-21 05:00:44 +0200

    .

--- run_class.sh
+++ run_class.sh
@@ -1,7 +1,7 @@
 python run_seq_class.py \
 --model_name "bert-base-uncased" \
 --learning_rate 1e-5 \
---num_train_epochs 5 \
+--num_train_epochs 4 \
 --per_device_train_batch_size 24 \
 --per_device_eval_batch_size 16 \
 --train_file "./data/Non-Disparagement/train.csv" \
@@ -10,8 +10,17 @@ python run_seq_class.py \
 python run_seq_class.py \
 --model_name "bert-base-uncased" \
 --learning_rate 1e-5 \
---num_train_epochs 5 \
+--num_train_epochs 4 \
 --per_device_train_batch_size 24 \
 --per_device_eval_batch_size 16 \
 --train_file "./data/Non-Disparagement/oversampled-train.csv" \
+--test_file "./data/Non-Disparagement/test.csv" \
+
+python run_seq_class.py \
+--model_name "bert-base-uncased" \
+--learning_rate 1e-5 \
+--num_train_epochs 4 \
+--per_device_train_batch_size 24 \
+--per_device_eval_batch_size 16 \
+--train_file "./data/Non-Disparagement/augmented-train.csv" \
 --test_file "./data/Non-Disparagement/test.csv" \
