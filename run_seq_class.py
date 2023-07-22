abc72f80b1488f609ebf924523cf2b935bb8581b
frank <kuete.frank1@gmail.com>
2023-07-22 18:49:45 +0200

    final changes in run_class.sh script / ready for running

--- run_seq_class.py
+++ run_seq_class.py
@@ -208,7 +208,7 @@ if __name__ == '__main__':
         os.mkdir("results")
     if not os.path.exists("results/"+category_name):
         os.mkdir("results/"+category_name)
-    if not os.path.exists("results/"+category_name+"/"+args.model_name):
+    if not os.path.exists("results/"+category_name+"/"+args.model_name.split("/")[-1]):
         os.mkdir("results/"+category_name+"/"+args.model_name)
     with open(save_path, 'w') as f:
         json.dump(predictions.metrics, f)
